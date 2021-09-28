05c35ad51950ad2b00eb1446806c9f9f9b79546e
prakashrao1977 <prakashrao1977@gmail.com>
2021-09-28 16:45:08 +0530

    removed comment

--- src/main/java/com/microservices/department/service/controller/DepartmentController.java
+++ src/main/java/com/microservices/department/service/controller/DepartmentController.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/departments")
 @Slf4j
 public class DepartmentController {
-	// Test commented entered now
 	@Autowired
 	private DepartmentService departmentService;
 	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DepartmentService.class);
