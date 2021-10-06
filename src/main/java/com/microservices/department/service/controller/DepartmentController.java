da3a39e8f8f44f8053fbbb2dc0a52ce1dcb4250d
Prakash <prakash.rao@hotmail.com>
2021-10-06 09:13:40 +0530

    Testing commit again-1

--- src/main/java/com/microservices/department/service/controller/DepartmentController.java
+++ src/main/java/com/microservices/department/service/controller/DepartmentController.java
@@ -24,6 +24,7 @@ public class DepartmentController {
 	@PostMapping("/")
 	public Department saveDepartment (@RequestBody Department department)
 	{
+	    // Testing commit again-1
 		log.info("DepartmentController.saveDepartment");
 		return departmentService.saveDepartment(department);
 	}
