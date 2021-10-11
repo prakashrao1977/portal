0524f5a1ff8d738b931cd0f19a7185a86bf0dfa8
Prakash <prakash.rao@hotmail.com>
2021-10-11 19:33:25 +0530

    Test comment again-4

--- rest/src/main/java/com/microservices/department/service/controller/DepartmentController.java
+++ rest/src/main/java/com/microservices/department/service/controller/DepartmentController.java
@@ -24,7 +24,7 @@ public class DepartmentController {
 	@PostMapping("/")
 	public Department saveDepartment (@RequestBody Department department)
 	{
-	    // Test Comment again-3
+	    // Test Comment again-4
 		log.info("DepartmentController.saveDepartment");
 		return departmentService.saveDepartment(department);
 	}
